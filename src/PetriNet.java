abe68ba8173490861d86e28b928e8fffcfe6bf6a
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2023-10-12 12:38:37 +0200

    Commentaires en anglais
    J'ai traduit quelques commentaires en anglais (pour les bonnes pratiques, tout ça), et j'ai rajouté des questions sur le code en français (comme ça on saura que français = commentaires entre nous)

--- src/PetriNet.java
+++ src/PetriNet.java
@@ -2,18 +2,16 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PetriNet implements PetriNetInterface {
-	
-	List<Place> listOfPlaces;
-	List<Edge> listOfEdges;
-	List<Transition> listOfTransitions;
-	
+
+	private List<Place> listOfPlaces;
+	private List<Edge> listOfEdges;
+	private List<Transition> listOfTransitions;
+
 	public PetriNet() {
 		listOfPlaces = new LinkedList<>();
 		listOfEdges = new LinkedList<>();
 		listOfTransitions = new LinkedList<>();
 	}
-	
-	
 
 	@Override
 	public void addPlace(Place placeToAdd) {
@@ -49,26 +47,27 @@ public class PetriNet implements PetriNetInterface {
 	}
 
 	@Override
-	public void removePlace(Place placeToRemove) {		
+	public void removePlace(Place placeToRemove) {
 		// on doit supprimer les edges reliés à la place (donc j'ai rajouté un getter)
-		for (int i = 0; i<placeToRemove.getLinkedEdgesList().size();++i) {
-			this.listOfEdges.remove(placeToRemove.getLinkedEdgesList().get(i)); // on le supprime du PetriNet
-			placeToRemove.removeEdgeFromLinkedEdges(placeToRemove.getLinkedEdgesList().get(i)); // on supprime les edges reliés à la place
-			
-			// on doit supprimer les même edges reliés à la transistion 
-			// (je pars du postulat pas très opti de regarder toutes les transistions 
+		// We delete every edge linked to the place to be removed
+		for (int i = 0; i < placeToRemove.getLinkedEdgesList().size(); ++i) {
+			this.listOfEdges.remove(placeToRemove.getLinkedEdgesList().get(i)); // Remove the edge from the PetriNet
+			placeToRemove.removeEdgeFromLinkedEdges(placeToRemove.getLinkedEdgesList().get(i)); // Remove the edge from the Place
+
+			// on doit supprimer les même edges reliés à la transistion
+			// (je pars du postulat pas très opti de regarder toutes les transistions
 			// et regarder si dans une transistion on a l'Edge associé)
-			for (int j = 0; j<this.listOfTransitions.size();++j) {
+			for (int j = 0; j < this.listOfTransitions.size(); ++j) {
 				this.listOfTransitions.get(j).removeInputEdgeFromInputEdges(placeToRemove.getLinkedEdgesList().get(i));
 				j--;
 			}
-			
+
 			// Décrémenter l'indice pour éviter de sauter un élément
 			i--;
 		}
-		
-		// puis on supprime la place 
-		this.listOfPlaces.remove(placeToRemove);  
+
+		// puis on supprime la place
+		this.listOfPlaces.remove(placeToRemove);
 	}
 
 	@Override
@@ -94,37 +93,42 @@ public class PetriNet implements PetriNetInterface {
 
 	@Override
 	public void removeTransition(Transition transitionToRemove) {
-		
+
 		// on doit remove les edges liées aussi mais pas forcement les places
-		for(int i = 0; i<transitionToRemove.getLinkedInputEdgesList().size();++i) {
+		for (int i = 0; i < transitionToRemove.getLinkedInputEdgesList().size(); ++i) {
 			// d'abord les InputEdges
 			this.listOfEdges.remove(transitionToRemove.getLinkedInputEdgesList().get(i));
 			// puis les OutputEdges
 			this.listOfEdges.remove(transitionToRemove.getLinkedOutputEdgesList().get(i));
-			
+
 			transitionToRemove.removeInputEdgeFromInputEdges(transitionToRemove.getLinkedOutputEdgesList().get(i));
 			transitionToRemove.removeOutputEdgeFromOutputEdges(transitionToRemove.getLinkedOutputEdgesList().get(i));
 			i--;
 		}
-		this.listOfTransitions.remove(transitionToRemove); 
+		this.listOfTransitions.remove(transitionToRemove);
 	}
-
 
 	@Override
 	public void linkPlaceWithTransistion(Place p, Transition t, Edge e) {
 		// TODO Auto-generated method stub
 		// pour le moment je n'ai pas l'impression que ce soit necessaire ...
 	}
-	
+
 	public String toString() {
 		String afficherPetriNet = "places : ";
-		
-		for (int i = 0; i<this.listOfPlaces.size();++i) afficherPetriNet += listOfPlaces.toString();
+
+		for (int i = 0; i < this.listOfPlaces.size(); ++i) {
+			afficherPetriNet += listOfPlaces.toString();
+		}
 		afficherPetriNet += " avec comme Edges : ";
-		for (int i = 0; i<this.listOfEdges.size();++i) afficherPetriNet += listOfEdges.toString();
+		for (int i = 0; i < this.listOfEdges.size(); ++i) {
+			afficherPetriNet += listOfEdges.toString();
+		}
 		afficherPetriNet += " avec comme Transistions : ";
-		for (int i = 0; i<this.listOfTransitions.size();++i) afficherPetriNet += listOfTransitions.toString();
-		
+		for (int i = 0; i < this.listOfTransitions.size(); ++i) {
+			afficherPetriNet += listOfTransitions.toString();
+		}
+
 		return "Notre réseau de PetriNet est composé de " + afficherPetriNet;
 	}
 
